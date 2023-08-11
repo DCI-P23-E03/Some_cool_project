19b2a4123371c7eae9833195d595c36c9be68b59
Nizar Chaouachi <nizar.chaouachi-freelancer@digitalcareerinstitute.org>
2023-08-11 12:31:25 +0100

    secret keys for everyone

--- some_cool_file.py
+++ some_cool_file.py
@@ -1,4 +1,13 @@
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-api_key='some cool api key'
+# loading every .env file in the project
+api_key=os.environ.get('API_KEY1')
+sk=os.getenv('SECRET_KEY')
+jk=os.getenv('JOHN_KEY')
 print(api_key)
+print(sk)
+print(jk)
+
