07ba01775054b3841d20e1283586b064ef4d218a
Nizar Chaouachi <nizar.chaouachi-freelancer@digitalcareerinstitute.org>
2023-08-11 13:25:28 +0100

    nizar did it again

--- some_cool_file.py
+++ some_cool_file.py
@@ -5,7 +5,7 @@ from sergey_secret_key import SERGEY_SK,hello
 load_dotenv()
 
 # loading every .env file in the project
-api_key=os.environ.get('API_KEY1')
+
 sk=os.getenv('SECRET_KEY')
 jk=os.getenv('JOHN_KEY')
 ssk=SERGEY_SK
